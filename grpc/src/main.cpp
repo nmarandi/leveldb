ca19de34ec8fb0647783bc8004dd2c6659b495c8
nader marandi <nader.marandi@samim.net>
2021-08-14 16:15:37 +0430

    add grpc base model

---
+++ grpc/src/main.cpp
@@ -0,0 +1,9 @@
+#include "leveldb/db.h"
+
+int main(int argc, char *argv[])
+{
+    leveldb::DB* db;
+    leveldb::Options options;
+    options.create_if_missing = true;
+    leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
+}
